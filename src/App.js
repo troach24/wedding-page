926a524a3200d1cf41629ed1f7aab69800937df5
Travis Roach <travisroach@journeyapps.com>
2022-08-31 20:38:24 -0600

    add link to wedding site

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button type="button" class="btn btn-dark">Subscribe to Wedding Updates (👋 Recommended)</button>
+        <button type="button" className="btn btn-dark">Subscribe to Wedding Updates (👋 Recommended)</button>
         <br />
-        <button type="button" class="btn btn-dark">Visit Our Official Wedding Site</button>
+        <a href="https://zola.com/wedding/alex-y-travis" target="_blank" rel="noreferrer">
+          <button type="button" className="btn btn-dark" onClick={test}>Visit Our Official Wedding Site</button>
+        </a>
         <br />
-        <button type="button" class="btn btn-dark">Download Wedding Guide (PDF)</button>
+        <button type="button" className="btn btn-dark">Download Wedding Guide (PDF)</button>
       </header>
     </div>
   );
+}
+
+function test() {
+  console.log('TEST');
 }
 
 export default App;
