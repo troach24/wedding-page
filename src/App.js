3fc45b2394a26cf2480959d5e383a3fd1249272a
Travis Roach <travisroach@journeyapps.com>
2022-09-27 21:21:34 -0600

    integration working ;)

--- src/App.js
+++ src/App.js
@@ -3,39 +3,65 @@ import ringBride from './assets/ring-bride.png';
 import ringGroom from './assets/ring-groom.png';
 import './App.css';
 import * as Journey from './journey';
+import { useState } from 'react';
+
+function Form() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  return <div>
+    <br />
+    <input type="text" value={name} onChange={e=> setName(e.target.value)} placeholder="Name" class="input input-bordered w-full max-w-xs" />
+    <input type="text" value={email} onChange={e=> setEmail(e.target.value)} placeholder="Email" class="input input-bordered w-full max-w-xs" />
+    <input type="text" value={phone} onChange={e=> setPhone(e.target.value)} placeholder="Phone Number" class="input input-bordered w-full max-w-xs" />
+    <br />
+    <div>
+      <button class="btn" onClick={() => submitForm(name, email, phone)}>SUBMIT</button>
+    </div>
+  </div>
+}
+
+async function submitForm(name, email, phone) {
+  console.log('sending signup request..');
+  await Journey.sendSubscriptionRequest({
+    name: name,
+    email: email,
+    phone: phone
+  });
+}
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  function showSignupForm() {
+    setShowModal(!showModal);
+  }
+
+  function openPDF() {
+    window.open('./sample.pdf');
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <div className='ring-container'>
-          <img src={ringGroom} className="App-logo" alt="logo" Style="margin-right: -175px;" />
-          <img src={ringBride} className="App-logo-right" alt="logo" />
+          {/* <img src={ringGroom} className="App-logo" alt="logo" Style="margin-right: -175px;" />
+          <img src={ringBride} className="App-logo-right" alt="logo" /> */}
+          <img className="mask mask-circle" src="./t-a.jpg" alt='main' />
         </div>
         <br />
-        <button type="button" className="btn btn-dark" onClick={signupRequest}>Subscribe to Wedding Updates</button>
+        <button type="button" className="btn btn-dark" onClick={showSignupForm}>Subscribe to Wedding Updates</button>
+        {showModal ? (<Form />) : (<></>)}
         <br />
-        <div>
-
-        </div>
         <a href="https://zola.com/wedding/alex-y-travis" target="_blank" rel="noreferrer">
           <button type="button" className="btn btn-dark">Visit Our Official Wedding Site</button>
         </a>
         <br />
-        <button type="button" className="btn btn-dark">Download Wedding Guide (PDF)</button>
+        <button type="button" className="btn btn-dark" onClick={openPDF}>Download Wedding Guide (PDF)</button>
       </header>
     </div>
   );
 }
-
-async function signupRequest() {
-  console.log('sending signup request..');
-  await Journey.get();
-}
-
-// function showSubscriptionForm() {
-
-// }
-
 
 export default App;
--- src/journey.js
+++ src/journey.js
@@ -1,18 +1,16 @@
 const BASE_URL = "https://alex-y-travis-testing.poweredbyjourney.com";
 
-export async function get() {
+export async function sendSubscriptionRequest(body) {
     let signupRequest = await fetch(`${BASE_URL}/subscribe`, {
+        method: 'POST',
         headers: {
             "Authorization": process.env.TOKEN,
             "Content-Type": "Application/json",
             "Access-Control-Allow-Origin": "*"
-        }
+        },
+        body: JSON.stringify(body)
     })
 
     await signupRequest.json();
     console.log(signupRequest);
 }
-
-// module.exports = {
-//     get: get
-// }
